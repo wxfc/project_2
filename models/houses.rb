6ecd06154592d4483cb7aab8eb231a7695ea2eb0
Wayne Colaco <wcolaco@icloud.com>
2020-09-15 12:32:48 +1000

    more security updates

--- models/houses.rb
+++ models/houses.rb
@@ -1,4 +1,4 @@
-def run_sql(sql)
+def run_sql(sql, arr = [])
     db = PG.connect(ENV['DATABASE_URL'] || {dbname: 'ratethatagent'})
     results = db.exec(sql)
     db.close
@@ -10,27 +10,27 @@ def find_all_houses
 end
 
 def find_one_house_by_id(id)
-    house = run_sql("SELECT * FROM houses WHERE id = #{id};")
+    house = run_sql("SELECT * FROM houses WHERE id = $1;", [id])
     return house.first
 end
 
 def create_review(number_street, suburb, postcode, image_url, agent, review, user_id)
-    sql = "INSERT INTO houses (number_street, suburb, postcode, image_url, agent, review, user_id) VALUES ('#{number_street}', '#{suburb}', #{postcode}, '#{image_url}', '#{agent}', '#{review}', #{user_id});"
-    run_sql(sql)
+    sql = "INSERT INTO houses (number_street, suburb, postcode, image_url, agent, review, user_id) VALUES ($1, $2, $3, $4, $5, $6, $7);"
+    run_sql(sql, [number_street, suburb, postcode, image_url, agent, review, user_id])
 end
 
 def update_review(id, number_street, suburb, postcode, image_url, agent, review)
-    sql = "UPDATE houses SET number_street = '#{number_street}', suburb = '#{suburb}', postcode = #{postcode}, image_url = '#{image_url}', agent =  '#{agent}', review = '#{review}' WHERE id = #{id};"
-    run_sql(sql)
+    sql = "UPDATE houses SET number_street = '#{number_street}', suburb = '#{suburb}', postcode = #{postcode}, image_url = '#{image_url}', agent =  '#{agent}', review = '#{review}' WHERE id = $1;"
+    run_sql(sql, [id])
 end
 
 def destroy_review(id)
-    sql = "DELETE FROM houses WHERE id = #{id}"
-    run_sql(sql)
+    sql = "DELETE FROM houses WHERE id = $1";
+    run_sql(sql, [id])
 end
 
 def find_one_house_by_user_id(user_id)
-    house = run_sql("SELECT * FROM houses WHERE user_id = #{user_id};")
+    house = run_sql("SELECT * FROM houses WHERE user_id = $1;", [user_id])
     return house.first
 end
 
